9a8c4a5b326f279976691b68052860a1f89450d4
Jonathan Cotta <JonathanCotta@users.noreply.github.com>
2016-08-16 10:33:01 -0300

    gulpfile improvments

--- gulpfile.js
+++ gulpfile.js
@@ -6,35 +6,37 @@ concat = require('gulp-concat');
 rename = require('gulp-rename');
 brw = require('browser-sync').create();
 
-gulp.task("default", function(){
-  brw.init({
-    server: {baseDir:'./'},
-    ui: {port: 7070}
-  });
-
-  gulp.watch( "./assets/sass/**/*.scss",['sass']);
-  gulp.watch( "./app/*.js",['angular']);
-  gulp.watch("./index.html", ['sync']);
-  gulp.watch("./app/components/**/*.html", ['sync']);
-});
+gulp.task("default",['browserSync','watcher'] ,function(){});
 
 gulp.task("sass", function(){
   return gulp.src("./assets/sass/main.scss")
   .pipe(sass())
   .pipe(minify())
   .pipe(rename("style.min.css"))
-  .pipe(gulp.dest("./assets/css/"))
-  .pipe(brw.reload());
+  .pipe(gulp.dest("./assets/css/"));
 });
 
 gulp.task("angular", function(){
   return gulp.src(["./app/*.js","!./app/app.min.js"])
   .pipe(concat("app.min.js"))
   .pipe(uglify())
-  .pipe(gulp.dest("./app/"))
-  .pipe(brw.reload());
+  .pipe(gulp.dest("./app/"));
+});
+
+gulp.task("browserSync", function(){
+  return brw.init({
+    server: {baseDir:'./'},
+    ui: {port: 7070}
+  });
 });
 
 gulp.task("sync",function(){
   return brw.reload();
+});
+
+gulp.task("watcher",function(){
+  gulp.watch( "./assets/sass/**/*.scss",['sass' , 'sync']);
+  gulp.watch( "./app/*.js",['angular' , 'sync']);
+  gulp.watch("./index.html", ['sync']);
+  gulp.watch("./app/components/**/*.html", ['sync']);
 });
